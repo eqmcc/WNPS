37e9538345b272b2fc0a936e4d9fa14b30551d9b
eqmcc <xjtu.chr@gmail.com>
2013-05-16 06:43:14 -0400

    ok

--- wnps-0.26/wnps/.tmp_versions/wnps.mod
+++
@@ -1,2 +0,0 @@
-/home/wnps-0.26/wnps/wnps.ko
-/home/wnps-0.26/wnps/hook.o /home/wnps-0.26/wnps/kshell.o
--- wnps-0.26/wnps/.wnps.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wnps-0.26/wnps/wnps.o := ld -m elf_i386  -r -o /home/wnps-0.26/wnps/wnps.o /home/wnps-0.26/wnps/hook.o /home/wnps-0.26/wnps/kshell.o
